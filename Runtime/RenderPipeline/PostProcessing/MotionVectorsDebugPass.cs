c2d80505e02af582fb6b96fafa8a2f4a37cafeb5
AkiKurisu <2683987717@qq.com>
2026-01-06 23:57:16 +0800

    Add Motion Vectors Debug Pass support for RenderGraph
    Implemented Motion Vectors Debug Pass functionality within the RenderGraph framework for Unity 2023.1 or newer. This includes the addition of a new data structure for handling motion vector textures and integrating the rendering logic into the RenderGraph pipeline, enhancing debugging capabilities for motion vectors in the rendering process.

--- Runtime/RenderPipeline/PostProcessing/MotionVectorsDebugPass.cs
+++ Runtime/RenderPipeline/PostProcessing/MotionVectorsDebugPass.cs
@@ -1,7 +1,11 @@
 ﻿#if DEVELOPMENT_BUILD || UNITY_EDITOR
 using System;
+using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+#if UNITY_2023_1_OR_NEWER
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+#endif
 
 namespace Illusion.Rendering
 {
@@ -32,6 +36,47 @@ namespace Illusion.Rendering
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
+
+#if UNITY_2023_1_OR_NEWER
+        private class MotionVectorsDebugPassData
+        {
+            internal Material MotionVectorDebugMaterial;
+            internal TextureHandle MotionVectorColor;
+            internal TextureHandle ColorDestination;
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, FrameResources frameResources,
+            ref RenderingData renderingData)
+        {
+            ref var cameraData = ref renderingData.cameraData;
+            UniversalRenderer renderer = (UniversalRenderer)cameraData.renderer;
+
+            // Try to get motion vector texture from renderer resources first (preferred for RenderGraph)
+            TextureHandle motionVectorColorHandle = TextureHandle.nullHandle;
+            var motionVectorFromResources = renderer.resources.GetTexture(UniversalResource.MotionVectorColor);
+            if (!motionVectorFromResources.IsValid()) return;
+
+            
+            TextureHandle colorTargetHandle = renderer.activeColorTexture;
+            
+            using (var builder = renderGraph.AddRasterRenderPass<MotionVectorsDebugPassData>("Motion Vectors Debug", 
+                out var passData, profilingSampler))
+            {
+                passData.MotionVectorDebugMaterial = _motionVectorDebugMaterial.Value;
+                passData.MotionVectorColor = builder.UseTexture(motionVectorColorHandle);
+                passData.ColorDestination = builder.UseTextureFragment(colorTargetHandle, 0);
+                
+                builder.AllowPassCulling(false);
+                builder.AllowGlobalStateModification(true);
+                
+                builder.SetRenderFunc(static (MotionVectorsDebugPassData data, RasterGraphContext context) =>
+                {
+                    data.MotionVectorDebugMaterial.SetTexture(IllusionShaderProperties._MotionVectorTexture, data.MotionVectorColor);
+                    Blitter.BlitTexture(context.cmd, new Vector4(1, 1, 0, 0), data.MotionVectorDebugMaterial, 0);
+                });
+            }
+        }
+#endif
 
         public void Dispose()
         {
