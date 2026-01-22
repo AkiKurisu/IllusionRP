6f0c7b3a35f2b1edc515bff9cb2225b6df667f38
AkiKurisu <111233991+AkiKurisu@users.noreply.github.com>
2026-01-22 22:12:40 +0800

    Fix Vulkan UnpackNormal error

--- ShaderLibrary/NormalBuffer.hlsl
+++ ShaderLibrary/NormalBuffer.hlsl
@@ -13,7 +13,7 @@ void GetNormal(uint2 positionSS, out float3 N)
 {
 #ifdef _DEFERRED_RENDERING_PATH
     half4 gbuffer2 = LOAD_TEXTURE2D_X(_GBuffer2, positionSS);
-    N = normalize(UnpackNormal(gbuffer2.xyz));
+    N = normalize(UnpackNormal(gbuffer2));
 #else
     N = LoadSceneNormals(positionSS);
 #endif
